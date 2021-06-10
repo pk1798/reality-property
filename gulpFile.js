ef132c8f8cb35c39c1f6514c4ae8f5464237450a
Praveen Kumar <pk1798.kumar@gmail.com>
2021-06-10 15:03:23 +0530

    increased script loading

--- gulpFile.js
+++ gulpFile.js
@@ -3,6 +3,9 @@ const sass = require("gulp-sass");
 const browsersync = require("browser-sync");
 const { stream } = require("browser-sync");
 const imagemin = require("gulp-imagemin");
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 
 function style() {
   return gulp
@@ -19,6 +22,18 @@ function compress() {
     .pipe(gulp.dest("./dist/img/"));
 }
 
+//script paths
+var jsFiles = 'src/js/**/*.js',
+    jsDest = 'dist/js';
+
+function scripts(){
+  return gulp.src(jsFiles)
+  .pipe(concat('scripts.js'))
+  .pipe(rename('scripts.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest(jsDest));
+}
+
 function watch() {
   browsersync.init({
     server: {
@@ -27,10 +42,12 @@ function watch() {
   });
   gulp.watch("./src/sass/**/*.scss", style);
   gulp.watch("./src/img/**",compress);
+  gulp.watch("./src/js/**/*.js",scripts);
   gulp.watch("./dist/*.html").on("change", browsersync.reload);
 }
 
 exports.style = style;
 exports.watch = watch;
 exports.compress = compress;
+exports.scripts = scripts;
 
---
+++ src/js/glide.js
@@ -0,0 +1,19 @@
+//glide - carousel
+new Glide('.glide').mount()
+
+const config ={
+    type: 'carousel',
+    gap: 0,
+    focusAt: 'center',
+    perView: 3,
+    breakpoints: {
+        1200: {
+          perView: 1
+        },
+        700:{
+            perView: 1
+        }
+      }
+}
+
+new Glide('.glide', config).mount()
